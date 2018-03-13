f07d368bf6498d8733543a8542d19c450aa9c1b4
Andrey Bulava <abulava@gmail.com>
2018-03-13 21:54:27 +0300

    Get used to runtime environment variables.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import runtimeEnv from '@mars/heroku-js-runtime-env';
 
 class App extends Component {
   render() {
+    // Load the env object.
+    const env = runtimeEnv();
+
     return (
       <div className="App">
         <header className="App-header">
@@ -11,7 +15,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <code>Runtime env var example: { env.REACT_APP_HELLO }</code>
         </p>
       </div>
     );
